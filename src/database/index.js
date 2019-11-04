0c3917ac992c2d624a739ab6ed49985dc9859ac2
Jessi Leandro Castro <JLC@localhost.localdomain>
2019-11-04 13:52:14 -0300

    Fix database

--- src/database/index.js
+++ src/database/index.js
@@ -1,25 +1,40 @@
-const Sequelize = require("sequelize");
-const models = require("./models");
+// const Sequelize = require("sequelize");
+// const models = require("./models");
 
-const sequelize = new Sequelize({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
-  username: process.env.DB_USERNAME,
-  database: process.env.DB_DATABASE,
-  dialect: "postgres",
-  // operatorsAliases: false,
-  logging: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  define: {
-    freezeTableName: true,
-    paranoid: true
-  }
+// const sequelize = new Sequelize({
+//   host: process.env.DB_HOST || "localhost",
+//   port: process.env.DB_PORT,
+//   password: process.env.DB_PASSWORD,
+//   username: process.env.DB_USERNAME,
+//   database: process.env.DB_DATABASE,
+//   dialect: "postgres",
+//   // operatorsAliases: false,
+//   logging: false,
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   },
+//   define: {
+//     freezeTableName: true,
+//     paranoid: true
+//   }
+// });
+
+// const modelInstances = models.map(model => model(sequelize));
+// modelInstances.forEach(
+//   modelInstance =>
+//     modelInstance.associate && modelInstance.associate(sequelize.models)
+// );
+
+// module.exports = sequelize;
+
+const models = require("./models");
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres"
 });
 
 const modelInstances = models.map(model => model(sequelize));
